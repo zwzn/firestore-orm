1dbf74c525cb452f1a89a525ab50eaf021e4c5ee
Adam Bibby <adam_bibby@hotmail.com>
2020-02-24 09:40:04 -0500

    add useQueryChanges

--- src/hook.ts
+++ src/hook.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import { Model } from './model'
 import { QueryBuilder } from './query-builder'
 
@@ -6,6 +6,17 @@ export function useQuery<T extends Model>(q: QueryBuilder<T>): T[] | undefined {
     const [list, changeList] = useState<T[]>()
 
     useEffect(() => q.subscribe(changeList), [q.hash()])
+
+    return list
+}
+
+export function useQueryChanges<T extends Model>(q: QueryBuilder<T>): [T[], Map<string, string>] | undefined {
+    const [list, changeList] = useState<[T[], Map<string, string>]>()
+    const change = useCallback((list: T[], changes: Map<string, string>) => {
+        changeList([list, changes])
+    }, [changeList])
+
+    useEffect(() => q.subscribe(change), [q.hash()])
 
     return list
 }
